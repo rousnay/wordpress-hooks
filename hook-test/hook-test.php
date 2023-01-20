05f459000c0b8f8e4c861a4416ac8018a1be52f2
Mozahidur Rahman <mr.rousnay@gmail.com>
2023-01-21 04:09:12 +0600

    Hooks: add_filter

--- hook-test/hook-test.php
+++ hook-test/hook-test.php
@@ -5,10 +5,19 @@
  *
  */
 
-
+// Add text to login screen
 function hello_world()
 {
     echo 'Hello World';
 }
-
+//Example of using add_aciton
 add_action('login_header', 'hello_world');
+
+//Modify URL on login screen
+function change_headerurl($url)
+{
+    $url = "https://wittyplex.com";
+    return $url;
+}
+//Example of using add_filter
+add_filter('login_headerurl', 'change_headerurl',);
